dca4937161086a8645a31a3f12b7016b742beca8
liubaisong <1174306192@qq.com>
2021-10-26 21:27:59 +0800

    deploy

---
+++ precache-manifest.4074cfcd934edc841f8a4d0aa6183699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b43cef19166cde766d",
+    "url": "/morney-website/css/app.043dc600.css"
+  },
+  {
+    "revision": "cdb554ea5f85f3e5f853f75d388ea1a6",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "95b43cef19166cde766d",
+    "url": "/morney-website/js/app.afafe0c4.js"
+  },
+  {
+    "revision": "6ce769a70153d3d85f91",
+    "url": "/morney-website/js/chunk-vendors.3a4369e6.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "89aacef6d010bb66b34e908610b05e03",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
